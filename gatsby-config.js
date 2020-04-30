463dc5364ddfef7ded9368e644b4121b43e14580
Vu Thai Duy <vuthaiduy1990@gmail.com>
2020-04-30 10:49:09 +0700

    refs #1 - [React][Eslint]: setup eslint using eslint-config-airbnb Linting
    * https://www.gatsbyjs.org/packages/gatsby-plugin-eslint
    * Refer this post if eslint not work with Visual Studio Code
    https://stackoverflow.com/questions/57879267/create-react-app-subfolder-projects-do-not-lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,17 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-emotion`,
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
   ],
-}
+};
