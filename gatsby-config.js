274ce6174475a5a595c8d8d5f6052e79a678b287
Vu Thai Duy <vuthaiduy1990@gmail.com>
2020-04-30 10:52:01 +0700

    refs #1 - [React][Internationalization]: support internationalization
    * Usage
    import { useTranslation } from 'react-i18next';
    const { t } = useTranslation();
    <span>{t('copyright')}</span>

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-emotion`,
+    'gatsby-plugin-sass',
+    'gatsby-plugin-emotion',
     {
       resolve: 'gatsby-plugin-eslint',
       options: {
---
+++ i18n.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './lang/en.json';
+import vi from './lang/vi.json';
+
+// the translations
+// (tip move them in a JSON file and import them)
+const resources = {
+  en: {
+    translation: en,
+  },
+  vi: {
+    translation: vi,
+  },
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: 'en',
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
