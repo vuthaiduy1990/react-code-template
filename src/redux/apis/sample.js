335fbab040bf448a6bdf9dee0b5b91697d060c64
Vu Thai Duy <vuthaiduy1990@gmail.com>
2020-05-28 15:24:40 +0700

    refs #1 - [React][Environment][API]: Configure axios default url using environment variable
    * Usage
    ```
    # add variable in .env.development file
    GATSBY_DUMMY_API_URL=http://dummy.restapiexample.com/
    
    # In javascript code
    process.env.GATSBY_DUMMY_API_URL
    ```

--- src/redux/apis/sample.js
+++ src/redux/apis/sample.js
@@ -9,7 +9,10 @@ export const getEmployees = () => {
   return axios({
     method: 'get',
     url: '/api/v1/employees',
-    baseURL: 'http://dummy.restapiexample.com/',
+
+    // this url has been configure as default.
+    // @ee gastby-browser.js
+    // baseURL: 'http://dummy.restapiexample.com/',
   }).then(res => {
     if (res.status === HttpStatus.OK) {
       return res.data.data;
