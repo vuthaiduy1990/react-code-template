da870bbd7db394f7955779e800277a41b6e88ff7
Vu Thai Duy <vuthaiduy1990@gmail.com>
2020-05-31 20:25:29 +0700

    refs #1 - [React][Chatting][WebSocket]: use WebSocket to connect to Dummy Chat Server
    * usage
    ```javascript
    const socketRef = useRef(); // use ref to keep reference to socket instance.
    const initWebSocket = useCallback(() => {
      const ws = new WebSocket(process.env.GATSBY_DUMMY_WS_URL);
      socketRef.current = ws;
      ws.onopen = () => {
        // Listen for messages
        socketRef.current.onmessage = onMessageReceiving;
    
        // listen the error
        socketRef.current.onerror = onSocketError;
    
        // Listen for socket closes
        socketRef.current.onclose = onSocketClose;
      };
    }, [onMessageReceiving, onSocketClose, onSocketError]);
    
    ```

--- src/components/chatting/index.js
+++ src/components/chatting/index.js
@@ -1,5 +1,5 @@
-import React, { memo, useState, useCallback } from 'react';
-import Proptypes from 'prop-types';
+import React, { memo, useState, useCallback, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 import cls from 'classnames';
 
 import { Popover, Input } from 'antd';
@@ -8,26 +8,142 @@ import { faGhost } from '@fortawesome/free-solid-svg-icons';
 
 import css from './styles.module.scss';
 
-const { TextArea } = Input;
-
-const ChatForm = ({ onSending }) => {
+const hint = 'Type "disconnect" to test closing socket.\nType "error" to test socket error and reconnect';
+const ChatForm = ({ inputRef, textRef, onSending }) => {
   return (
     <>
-      <Input placeholder="Type something here" onKeyDown={onSending} className={css.sender} />
-      <TextArea
-        placeholder="Autosize height with minimum and maximum number of lines"
-        autoSize={{ minRows: 3, maxRows: 6 }}
-        disabled
-      />
+      <Input ref={inputRef} placeholder="Type something here" onKeyDown={onSending} className={css.sender} />
+      <textarea ref={textRef} placeholder={hint} rows="4" readOnly className={css.receiver} />
     </>
   );
 };
 ChatForm.propTypes = {
-  onSending: Proptypes.func.isRequired,
+  inputRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func]).isRequired,
+  textRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func]).isRequired,
+  onSending: PropTypes.func.isRequired,
 };
 
 const Chatting = ({ iconCss, iconSize = '2x', placement }) => {
+  const inputRef = useRef();
+  const textRef = useRef();
+  const socketRef = useRef(); // use ref to keep reference to socket instance.
   const [visible, setVisible] = useState(false);
+
+  // on component did mount
+  useEffect(() => {
+    initWebSocket();
+
+    // on component will un-mount
+    return () => {
+      disconnectSocket();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  /**
+   * Init web socket
+   */
+  const initWebSocket = useCallback(() => {
+    const ws = new WebSocket(process.env.GATSBY_DUMMY_WS_URL);
+    socketRef.current = ws;
+    ws.onopen = () => {
+      // Listen for messages
+      socketRef.current.onmessage = onMessageReceiving;
+
+      // listen the error
+      socketRef.current.onerror = onSocketError;
+
+      // Listen for socket closes
+      socketRef.current.onclose = onSocketClose;
+    };
+  }, [onMessageReceiving, onSocketClose, onSocketError]);
+
+  /**
+   * Disconnect socket normally
+   */
+  const disconnectSocket = useCallback(() => {
+    // close normally
+    // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
+    if (socketRef.current) {
+      socketRef.current.close(1000);
+    }
+  }, []);
+
+  /**
+   * Listener for socket error
+   */
+  const onSocketError = useCallback(
+    event => {
+      writeMessage(`Error ... ${event.code}`);
+    },
+    [writeMessage]
+  );
+
+  /**
+   * Listen for socket close
+   */
+  const onSocketClose = useCallback(
+    event => {
+      if (event.code === 1000) {
+        // Error code 1000 means that the connection was closed normally.
+        writeMessage('Closed ...');
+      } else {
+        // The onerror event is fired when something wrong occurs between the communications.
+        // The event onerror is followed by a connection termination, which is a close event.
+        // Try to reconnect.
+        writeMessage('Reconnect ...');
+        initWebSocket();
+      }
+    },
+    [initWebSocket, writeMessage]
+  );
+
+  /**
+   * Trigger when user
+   */
+  const onSending = useCallback(
+    e => {
+      const message = e.target.value;
+      const keyCode = e.which || e.keyCode;
+      if (keyCode === 13) {
+        // user press enter
+        // send value here to server
+        inputRef.current.setValue(''); // empty input
+        writeMessage(`Sending: ${message}`);
+        socketRef.current.send(message);
+
+        // send disconect or error for demoing purpose
+        if (message === 'disconnect') {
+          disconnectSocket();
+        } else if (message === 'error') {
+          socketRef.current.close();
+          socketRef.current.send('trigger errror');
+        }
+      }
+    },
+    [disconnectSocket, writeMessage]
+  );
+
+  /**
+   * Listen for messages
+   */
+  const onMessageReceiving = useCallback(
+    event => {
+      writeMessage(`Receiving: ${event.data} - ${event.timeStamp}`);
+    },
+    [writeMessage]
+  );
+
+  /**
+   * Write message
+   */
+  const writeMessage = useCallback(message => {
+    const textEl = textRef.current;
+    if (textEl) {
+      textEl.append(`${message}\n`);
+      textEl.scrollTop = textEl.scrollHeight;
+    }
+  }, []);
 
   /**
    * Trigger when user click on ghost icon to show chat form
@@ -36,21 +152,10 @@ const Chatting = ({ iconCss, iconSize = '2x', placement }) => {
     setVisible(!visible);
   }, [visible]);
 
-  /**
-   * Trigger when user
-   */
-  const onSending = useCallback(e => {
-    const keyCode = e.which || e.keyCode;
-    if (keyCode === 13) {
-      // user press enter
-      // send value here to server
-    }
-  }, []);
-
   return (
     <>
       <Popover
-        content={<ChatForm onSending={onSending} />}
+        content={<ChatForm inputRef={inputRef} textRef={textRef} onSending={onSending} />}
         title="Ask your ass."
         placement={placement}
         visible={visible}
@@ -62,8 +167,8 @@ const Chatting = ({ iconCss, iconSize = '2x', placement }) => {
 };
 
 Chatting.propTypes = {
-  iconCss: Proptypes.string,
-  iconSize: Proptypes.string,
-  placement: Proptypes.string,
+  iconCss: PropTypes.string,
+  iconSize: PropTypes.string,
+  placement: PropTypes.string,
 };
 export default memo(Chatting);
